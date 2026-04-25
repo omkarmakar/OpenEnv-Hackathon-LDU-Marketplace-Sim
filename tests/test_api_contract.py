1268e2a21bbf9f273a1094a33f7a5ceb914cedf7
Om Karmakar <omkarmakar07@gmail.com>
2026-04-25 09:09:31 +0530

    Test_api_Contract fix

--- tests/test_api_contract.py
+++ tests/test_api_contract.py
@@ -1,5 +1,8 @@
-from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from fastapi.testclient import TestClient
 from main import app
 
 
